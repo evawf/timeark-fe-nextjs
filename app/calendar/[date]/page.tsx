1c80392db385a5e68eddab3cebbb72cd4c56a0a2
eva.wang <eva.wang81@gmail.com>
2023-04-21 16:15:51 +0800

    removed global css

--- app/calendar/[date]/page.tsx
+++ app/calendar/[date]/page.tsx
@@ -10,6 +10,13 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+
+// Styling CSS
+import "./styles.css";
+// import bootstrap5Plugin from "@fullcalendar/bootstrap5";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 interface Date {
   date: string;
@@ -24,8 +31,8 @@ export default function PickedDate({ params }: Date | any) {
   const events = [
     {
       title: "event2",
-      start: "2023-04-18",
-      end: "2023-04-19",
+      start: "2023-04-18T16:00:00",
+      end: "2023-04-18T17:00:00",
     },
   ];
 
@@ -51,14 +58,25 @@ export default function PickedDate({ params }: Date | any) {
     <div>
       <p>Calendar current date: {date && <>{date}</>} page</p>
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          // bootstrap5Plugin,
+          listPlugin,
+        ]}
+        // themeSystem="bootstrap5"
+        themeSystem="bootstrap5"
         initialView="timeGridWeek"
         headerToolbar={{
-          center: "dayGridMonth,timeGridWeek,timeGridDay NEW",
+          // center: "dayGridMonth,timeGridWeek,timeGridDay NEW",
+          left: "prev,next today",
+          center: "NEW",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
         }}
         customButtons={{
           NEW: {
-            text: "NEW",
+            text: `${today}`,
             click: () => console.log("new event"),
           },
         }}
@@ -67,6 +85,7 @@ export default function PickedDate({ params }: Date | any) {
         nowIndicator
         dateClick={(e) => console.log(e.dateStr)}
         eventClick={(e) => console.log(e.event.title)}
+        height={"100vh"}
       />
     </div>
   );
