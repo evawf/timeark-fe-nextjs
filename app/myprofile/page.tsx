a7e82fd05b31a6ff96f593a55ca0c12f31e0d7d7
eva.wang <eva.wang81@gmail.com>
2023-05-31 22:03:14 +0800

    styled my profile page

--- app/myprofile/page.tsx
+++ app/myprofile/page.tsx
@@ -6,6 +6,15 @@ import { useRouter } from "next/navigation";
 import DeleteUser from "@/lib/DeleteUser";
 import Sidebar from "../components/Sidebar";
 
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
 export default function MyProfile() {
   const [userData, setUserData] = useState<User | any>();
   const [userId, setUserId] = useState("");
@@ -51,36 +60,335 @@ export default function MyProfile() {
   };
 
   return (
-    <div>
+    <Box sx={{ marginTop: "64px", display: "flex", flexDirection: "row" }}>
       <Sidebar />
-      {userData ? (
-        <>
-          user page:
-          <div>First Name: {userData.firstName}</div>
-          <div>Last Name: {userData.lastName}</div>
-          <div>Email: {userData.email}</div>
-          <div>Avatar: {userData.avatar}</div>
-          <div>Company Name: {userData.companyName}</div>
-          <div>Registration Number: {userData.registrationNumber}</div>
-          <div>Address: {userData.address}</div>
-          <div>City: {userData.city}</div>
-          <div>Country: {userData.country}</div>
-          <div>Postalcode:{userData.postalCode} </div>
-          <div>Contact: {userData.contact}</div>
-          <div>
-            <button onClick={() => router.push("/myprofile/update")}>
-              Update My Profile
-            </button>
-          </div>
-          <div>
-            <button onClick={() => deleteUserAccount()}>
-              Delete My Account
-            </button>
-          </div>
-        </>
-      ) : (
-        <>Loading</>
-      )}
-    </div>
+      <Box sx={{ width: "100%", margin: 2 }}>
+        {userData ? (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              mt: 5,
+            }}
+          >
+            <Card
+              sx={{
+                width: "80%",
+                margin: 1,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                // backgroundColor: "lightblue",
+                height: "auto",
+              }}
+            >
+              <CardContent>
+                <Box>Avatar: {userData.avatar}</Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignContent: "space-between",
+                    my: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      First Name:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.firstName}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Last Name:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.lastName}
+                    </Typography>
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignContent: "space-between",
+                    my: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Company Name:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.companyName}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Registration Number:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.registrationNumber}
+                    </Typography>
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignContent: "space-between",
+                    my: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Address:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.address}
+                    </Typography>
+                  </Box>
+
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>City: </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.city}
+                    </Typography>
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignContent: "space-between",
+                    my: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Country:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.country}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Postalcode:
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {" "}
+                      {userData.postalCode}
+                    </Typography>
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignContent: "space-between",
+                    my: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Contact:{" "}
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {" "}
+                      {userData.contact}
+                    </Typography>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid black",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                    }}
+                  >
+                    <Typography sx={{ mx: 2, width: "40%" }}>
+                      Email:{" "}
+                    </Typography>
+                    <Typography sx={{ mx: 2, width: "60%" }}>
+                      {userData.email}
+                    </Typography>
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignContent: "space-between",
+                    my: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid blue",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Button
+                      color="primary"
+                      onClick={() => router.push("/myprofile/update")}
+                    >
+                      Update My Profile
+                    </Button>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      mx: 1,
+                      border: "1px solid red",
+                      height: "50px",
+                      borderRadius: "15px",
+                      alignItems: "center",
+                      width: "50%",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Button
+                      sx={{ color: "red" }}
+                      onClick={() => deleteUserAccount()}
+                    >
+                      Delete My Account
+                    </Button>
+                  </Box>
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+        ) : (
+          <>Loading</>
+        )}
+      </Box>
+    </Box>
   );
 }
